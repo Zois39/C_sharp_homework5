9fe451f41b4f1669ee23ac4402b2db9853d9ecda
konstantin bondarenko <zois1990@gmail.com>
2022-08-25 12:54:38 +0200

    task 34 redacted

--- Program.cs
+++ Program.cs
@@ -1,28 +1,40 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
 /*
 int size = new Random().Next(1, 10);
 
 int[] RandomArray (int length)
 {
+    // creates an array of 3-digit numbers of a given length
     int[] array = new int[length];
-    for(int i = 0; i < length; i++)
+    for(int i = 0; i < array.Length; i++)
         array[i] = new Random().Next(100, 1000);
     return array;
 }
 
 int CountEven (int[] arr)
 {
+    // counts quantity of even numbers in an array
     int result = 0;
     for(int i = 0; i < arr.Length; i++)
-    {
         if (arr[i] % 2 == 0)
             result++;
-    }
     return result;
 }
 
-Console.WriteLine(CountEven(RandomArray(size)));
+void ShowArray(int[] array)
+{
+    // Show all elements of array
+    Console.Write("Array:");
+    for(int i = 0; i < array.Length; i++)
+        Console.Write($" {array[i]}");
+    Console.WriteLine();
+}
+
+int[] newArray = RandomArray(size);
+ShowArray(newArray);
+Console.WriteLine($"Count even elements: {CountEven(newArray)}");
 */
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
